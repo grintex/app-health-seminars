5a2846c65a3edf1f845d7e85086cdec5dc7708d6
Fernando Bevilacqua <dovyski@gmail.com>
2020-08-17 09:56:14 -0300

    Remove unused stuff

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,6 +1,5 @@
 import HomePage from '../pages/home.f7.html';
-import InfectedQuizPage from '../pages/infected-quiz.f7.html';
-import RiskGroupQuizPage from '../pages/risk-group-quiz.f7.html';
+import ViewerPage from '../pages/viewer.f7.html';
 import NotFoundPage from '../pages/404.f7.html';
 
 var routes = [
@@ -9,27 +8,15 @@ var routes = [
         component: HomePage,
     },
 
-    // Page with symptoms quiz
     {
-        path: '/infected/:age/',
-        component: InfectedQuizPage,
+        path: '/viewer/:activity/:step/',
+        component: ViewerPage,
         options: {
             context: {
-                age: '{{age}}',
+                activity: '{{activity}}',
+                step: '{{step}}',
             },
         },
-    },
-
-    {
-        path: '/risk-group-quiz/:age/:symptoms',
-        component: RiskGroupQuizPage,
-        options: {
-            context: {
-                age: '{{age}}',
-                symptoms: '{{symptoms}}'
-            },
-        },
-
         on: {
             pageInit: function (event, page) {
                 console.log(page.route.params.age);
