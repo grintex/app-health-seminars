1d69e1db020b43d661d75ab675fd9e4a96fc095b
Fernando Bevilacqua <dovyski@gmail.com>
2020-08-19 16:42:45 -0300

    Improves in UX/UI + new help page + new about info

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,6 +1,7 @@
 import HomePage from '../pages/home.f7.html';
 import ViewerPage from '../pages/viewer.f7.html';
 import AboutPage from '../pages/about.f7.html';
+import HelpPage from '../pages/help.f7.html';
 import NotFoundPage from '../pages/404.f7.html';
 
 var routes = [
@@ -28,6 +29,11 @@ var routes = [
     {
         path: '/about/',
         component: AboutPage
+    },
+
+    {
+        path: '/help/',
+        component: HelpPage
     },
 
     {
