50645a55b16a3c55e9050448a8b7d9b74dce3ffe
Fernando Bevilacqua <dovyski@gmail.com>
2020-08-19 08:43:55 -0300

    Add about page

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,5 +1,6 @@
 import HomePage from '../pages/home.f7.html';
 import ViewerPage from '../pages/viewer.f7.html';
+import AboutPage from '../pages/about.f7.html';
 import NotFoundPage from '../pages/404.f7.html';
 
 var routes = [
@@ -22,6 +23,11 @@ var routes = [
                 console.log(page.route.params.activity, page.route.params.step);
             }
         }
+    },
+
+    {
+        path: '/about/',
+        component: AboutPage
     },
 
     {
